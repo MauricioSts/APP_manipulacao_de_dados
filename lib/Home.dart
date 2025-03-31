fa58c30976d8bc4927ca9fa18a08b9ef20989ab5
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-31 14:32:30 -0300

    entendeno shared preferences

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,7 +9,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _valorSalvo = "nada salvooo";
+
   TextEditingController _digitado = TextEditingController();
+
+  _salver() async {
+    String _valorDigitado = _digitado.text;
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString("nome", _valorDigitado);
+
+    print(_valorDigitado);
+  }
+
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _valorSalvo = prefs.getString("nome") ?? "nenhum valor salvo";
+    });
+  }
+
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("nome");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +42,29 @@ class _HomeState extends State<Home> {
       body: Container(
         child: Column(
           children: [
-            Text(""),
+            Text(_valorSalvo),
             TextField(
               decoration: InputDecoration(label: Text("Digite algo")),
               controller: _digitado,
             ),
-            TextButton(onPressed: () {}, child: Text("salvar")),
-            TextButton(onPressed: () {}, child: Text("ler")),
+            TextButton(
+              onPressed: () {
+                _salver();
+              },
+              child: Text("salvar"),
+            ),
+            TextButton(
+              onPressed: () {
+                _recuperar();
+              },
+              child: Text("recuperar"),
+            ),
+            TextButton(
+              onPressed: () {
+                _remover();
+              },
+              child: Text("remover"),
+            ),
           ],
         ),
       ),
